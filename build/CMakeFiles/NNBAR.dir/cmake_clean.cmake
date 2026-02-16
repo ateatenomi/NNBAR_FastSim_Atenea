40a1be9733f1f422aa937759ce9e03cb3fdac241
Atenea Utrilla Araoz <ateneaua@cosmos1.int.lunarc>
2026-02-16 14:30:58 +0100

    gitignore stuff

--- build/CMakeFiles/NNBAR.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/NNBAR"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/NNBAR.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
